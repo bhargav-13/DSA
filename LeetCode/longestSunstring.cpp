92afbe20a43a19a214dd6c2c03b093def0902edc
bhargav-13 <bhargavthesiya11@gmail.com>
2023-07-16 18:38:46 +0530

    16-07

---
+++ LeetCode/longestSunstring.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int lengthOfLongestSubstring(string s){
+    if(s.length()==0)return 0;
+        unordered_map<char,int> m; 
+        int i=0,j=0,ans=INT_MIN; 
+        while(j<s.length())   
+        {
+            m[s[j]]++;  
+            if(m.size()==j-i+1)  
+            {
+                ans = max(ans,j-i+1);  
+            }
+            else if(m.size()<j-i+1)   
+            {
+                while(m.size()<j-i+1) 
+                {
+                    m[s[i]]--;   
+                    if(m[s[i]]==0)  
+                    {
+                        m.erase(s[i]);
+                    }
+                    i++;
+                }
+            }
+             j++;
+        }
+    return ans;
+}
+
+int main(int argc, char const *argv[])
+{
+    string s = "abcabcbb";
+
+    cout << lengthOfLongestSubstring(s);
+    return 0;
+}
