2e691056cdf1c2fd2a6550e23a7fa0bf1a6ea71f
bhargav-13 <bhargavthesiya11@gmail.com>
2024-01-02 23:42:52 +0530

    2-1-2024

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -1,55 +1,42 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-void merge(int *arr, int s, int e) {
+void merge(vector<int> &arr, int low, int mid, int high ){
 
-    int mid = (s+e)/2;
+    vector<int> temp;
 
-    int len1 = mid - s + 1;
-    int len2 = e - mid;
+    int left = low;
+    int right = mid+1;
 
-    int *first = new int[len1];
-    int *second = new int[len2];
-
-    //copy values
-    int mainArrayIndex = s;
-    for(int i=0; i<len1; i++) {
-        first[i] = arr[mainArrayIndex++];
-    }
-
-    mainArrayIndex = mid+1;
-    for(int i=0; i<len2; i++) {
-        second[i] = arr[mainArrayIndex++];
-    }
-
-    //merge 2 sorted arrays     
-    int index1 = 0;
-    int index2 = 0;
-    mainArrayIndex = s;
-
-    while(index1 < len1 && index2 < len2) {
-        if(first[index1] < second[index2]) {
-            arr[mainArrayIndex++] = first[index1++];
+    while( left <= mid && right <= high ){
+        if(arr[left] <= arr[right]){
+            temp.push_back(arr[left]);
+            left++;
         }
         else{
-            arr[mainArrayIndex++] = second[index2++];
+            temp.push_back(arr[right]);
+            right++;
         }
-    }   
-
-    while(index1 < len1) {
-        arr[mainArrayIndex++] = first[index1++];
     }
 
-    while(index2 < len2 ) {
-        arr[mainArrayIndex++] = second[index2++];
+    while(left <= mid){
+        temp.push_back(arr[left]);
+            left++;
     }
 
-    delete []first;
-    delete []second;
+     while(right <= high){
+       temp.push_back(arr[right]);
+            right++;
+    }
+
+    for(int i = low; i<=high; i++){
+        arr[i] = temp[i-low];
+    }
 
 }
 
-void mergeSort(int *arr, int s, int e) {
+
+void mergeSort(vector<int> &arr, int s, int e) {
 
     //base case
     if(s >= e) {
@@ -65,13 +52,13 @@ void mergeSort(int *arr, int s, int e) {
     mergeSort(arr, mid+1, e);
 
     //merge
-    merge(arr, s, e);
+    merge(arr, s, mid,  e);
 
 }
 
 int main() {
 
-    int arr[15] = {3,7,0,1,5,8,3,2,34,66,87,23,12,12,12};
+    vector<int> arr = {3,7,0,1,5,8,3,2,34,66,87,23,12,12,12};
     int n = 15;
 
     mergeSort(arr, 0, n-1);
